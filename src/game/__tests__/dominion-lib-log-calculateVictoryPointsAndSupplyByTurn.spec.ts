9fdfdfd4907ea5dac5d3476912b2dcfec3e6b045
Jessica Mulein <jessica@digitaldefiance.org>
2024-11-04 01:36:20 +0000

    fix flaky test

--- src/game/__tests__/dominion-lib-log-calculateVictoryPointsAndSupplyByTurn.spec.ts
+++ src/game/__tests__/dominion-lib-log-calculateVictoryPointsAndSupplyByTurn.spec.ts
@@ -1,7 +1,7 @@
 import { calculateVictoryPointsAndSupplyByTurn } from '@/game/dominion-lib-log';
 import { IGameSupply } from '@/game/interfaces/game-supply';
 import { GameLogAction } from '@/game/enumerations/game-log-action';
-import { createMockGame } from '@/__fixtures__/dominion-lib-fixtures';
+import { createMockGame, createMockLog } from '@/__fixtures__/dominion-lib-fixtures';
 import { IGame } from '@/game/interfaces/game';
 import { ILogEntry } from '@/game/interfaces/log-entry';
 import { deepClone } from '@/game/utils';
@@ -147,9 +147,9 @@ describe('calculateVictoryPointsAndSupplyByTurn', () => {
     ]);
   });
 
-  it('should handle actions affecting multiple fields correctly', () => {
+  it.only('should handle actions affecting multiple fields correctly', () => {
     game.log.push(
-      {
+      createMockLog({
         id: '2',
         timestamp: new Date(),
         action: GameLogAction.ADD_ESTATES,
@@ -157,30 +157,47 @@ describe('calculateVictoryPointsAndSupplyByTurn', () => {
         currentPlayerIndex: 0,
         turn: 1,
         count: 1,
-      },
-      {
+      }),
+      createMockLog({
         id: '3',
+        action: GameLogAction.NEXT_TURN,
+        turn: 2,
+        playerIndex: 1,
+        currentPlayerIndex: 1,
+        prevPlayerIndex: 0,
+      }),
+      createMockLog({
+        id: '4',
         timestamp: new Date(),
         action: GameLogAction.ADD_VP_TOKENS,
         playerIndex: 1,
         currentPlayerIndex: 1,
         turn: 2,
         count: 2,
-      },
-      {
-        id: '4',
+      }),
+      createMockLog({
+        id: '5',
         timestamp: new Date(),
         action: GameLogAction.NEXT_TURN,
         playerIndex: 0,
         currentPlayerIndex: 0,
+        prevPlayerIndex: 1,
         turn: 3,
-      }
+      })
     );
+    game.currentTurn = 3;
 
     const result = calculateVictoryPointsAndSupplyByTurn(game);
 
-    expect(result).toHaveLength(1);
+    expect(result).toHaveLength(2);
     expect(result).toEqual([
+      {
+        playerScores: { 0: 4, 1: 3 },
+        supply: {
+          ...initialSupply,
+          estates: initialSupply.estates - 1,
+        },
+      },
       {
         playerScores: { 0: 4, 1: 5 },
         supply: {
